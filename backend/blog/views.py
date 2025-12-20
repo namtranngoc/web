dd6ca40132c2654dfa02c869b0429c383624031c
Your Name <you@example.com>
2025-12-20 15:43:01 +0700

    update

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -25,4 +25,4 @@ class ServiceList(generics.ListAPIView):
 class ServiceDetail(generics.RetrieveAPIView):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
-    lookup_field = 'id'
+    lookup_field = 'slug'
